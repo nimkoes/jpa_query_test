5ce3cc4dcb9e269fe293bbfe10bf29dc131651c3
seokmin <smhong@tradlinx.com>
2024-10-16 08:55:32 +0900

    initial commit

---
+++ src/main/java/io/github/nimkoes/jpa_query/JpaQueryApplication.java
@@ -0,0 +1,13 @@
+package io.github.nimkoes.jpa_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nimkoes/jpa_query/JpaQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nimkoes.jpa_query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
